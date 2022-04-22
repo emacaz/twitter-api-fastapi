84626ff262ba764b3341cca5c948e608f8ee459d
emanuel <ecastillo76@misena.edu.co>
2022-04-22 10:06:27 -0500

    path_operations_structure_users

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from uuid import UUID
 from datetime import date
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 
 # Pydantic
@@ -12,13 +12,15 @@ from pydantic import Field
 
 # FastAPI
 from fastapi import FastAPI
+from fastapi import status
 
 app = FastAPI()
 
 # Models
 
 class UserBase(BaseModel):
-    pass
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
 
 class UserLogin(UserBase):
     password: str = Field(
@@ -26,7 +28,6 @@ class UserLogin(UserBase):
         min_length=8,
         max_length=64,
     )
-
 
 class User(UserBase):
     user_id: UUID = Field(...)
@@ -55,6 +56,8 @@ class Tweet(BaseModel):
     updated_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 
+# Path Operations
+
 @app.get(
     path="/"
 )
@@ -62,3 +65,72 @@ def home():
     return {
         "Twitter API": "Working!"
     }
+
+## Users
+
+@app.post(
+    path="/signup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register a user",
+    tags=["Users"]
+)
+def signup():
+    pass
+
+
+@app.post(
+    path="/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login a user",
+    tags=["Users"]
+)
+def login():
+    pass
+
+
+@app.get(
+    path="/users",
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary="Show all users",
+    tags=["Users"]
+)
+def show_all_users():
+    pass
+
+
+@app.get(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Show a user",
+    tags=["Users"]
+)
+def show_a_user():
+    pass
+
+
+@app.delete(
+    path="/users/{user_id}/delete",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a user",
+    tags=["Users"]
+)
+def delete_a_user():
+    pass
+
+
+@app.put(
+    path="users/{user_id}/update",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Update a user",
+    tags=["Users"]
+)
+def update_a_user():
+    pass
+
+## Tweets
